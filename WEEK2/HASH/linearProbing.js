ac9f9a452b9a85a6e24f5c29b38feb3632605f45
Ashika <ashikasivan07@gmail.com>
2025-08-08 23:35:58 +0530

    implement linear probing

---
+++ WEEK2/HASH/linearProbing.js
@@ -0,0 +1,45 @@
+class HashTable {
+    constructor(size) {
+        this.table = new Array(size).fill(null);
+        this.size = size;
+    }
+
+    hash(key) {
+        return key % this.size;
+    }
+
+    insert(key, value) {
+        let index = this.hash(key);
+        let startIndex = index;
+
+        while (this.table[index] !== null && this.table[index].key !== key) {
+            index = (index + 1) % this.size;
+            if (index === startIndex) {
+                console.log("Hash table is full!");
+                return;
+            }
+        }
+        this.table[index] = { key, value };
+    }
+
+    search(key) {
+        let index = this.hash(key);
+        let startIndex = index;
+
+        while (this.table[index] !== null) {
+            if (this.table[index].key === key) {
+                return this.table[index].value;
+            }
+            index = (index + 1) % this.size;
+            if (index === startIndex) break;
+        }
+        return null;
+    }
+}
+
+let ht = new HashTable(5);
+ht.insert(1, "A");
+ht.insert(6, "B"); // collision, goes to next slot
+ht.insert(11, "C"); // collision chain
+console.log(ht.search(6)); // "B"
+console.log(ht.table);
